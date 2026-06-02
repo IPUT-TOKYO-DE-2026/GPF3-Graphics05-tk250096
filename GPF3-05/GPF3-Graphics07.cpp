b287f087e4c57c9abf9ea0a0cda8d071ab0675d2
Terakawa <tera.ra.2006@gmail.com>
2026-06-02 15:29:19 +0900

    6/2提出

--- GPF3-05/GPF3-Graphics07.cpp
+++ GPF3-05/GPF3-Graphics07.cpp
@@ -1,54 +1,54 @@
 ﻿#include "FrameBufferEmulator.h"
 
+int N = 6; 
+constexpr int MAX_CNT = 10000; //操作回数の最大許容値 
+int b[MAX_CNT] = { 1, 4, 1, 3, 2, 4 }; // 現在のブロックの各列の高さを格納する配列 
+int tmp[MAX_CNT]; // 操作の途中、一時的に新しい列の高さを格納するための作業用配列 
+int cnt; 
+unsigned char rectColor[] = { 0, 200, 0 }; 
+unsigned char gridColor[] = { 200, 200, 200 }; 
 
-//  buff フレームバッファの先頭アドレス
-
-//  width, height フレームバッファの高さと横幅
-//  radius 円の半径
-//  centerX, centerY 中心座標(X, Y)
-//  color 描画色（B,G,Rの配列）
-void drawFilledCircle(unsigned char* buff, int width, int height, int radius, int centerX, int centerY, unsigned char color[3])
+// 長方形を描画する 
+void drawRectangle(unsigned char* buff, int width, int height, int sx, int sy, int w, int h, unsigned char color[3]) 
 {
-	const int squaredRadius = radius * radius; // 半径の二乗
-	for (int y = 0; y < height; y++) { // 縦方向のループ
-		int squaredY = y - centerY;	// Y軸の中心からの距離
-		squaredY *= squaredY;  // 二乗しておく
-		for (int x = 0; x < width; x++) { // 横方向のループ
-			int squaredX = x - centerX;	// X軸の中心からの距離
-			squaredX *= squaredX;  // 二乗しておく
-			if (squaredY + squaredX <= squaredRadius) { // 距離が半径以下ならば（二乗どうしで比較）
-				// 現在のX,Yで示す位置は円の内側として色を置く
-				*buff++ = color[0];  // B
-				*buff++ = color[1];  // G
-				*buff++ = color[2];  // R
-			}
-			else {
-				buff += 3;	// 現在のX,Yで示す位置は円の外側（色は置かず次のピクセルに移る）
-			}
-		}
-	}
+	int ex = sx + w; 
+	int ey = sy + h; 
+	unsigned char* pixel; 
+	for (int i = sy; i < ey; i++) { // 縦方向のループ 
+		pixel = buff + i * width * 3 + sx * 3; // フレームバッファ上で1行書き始める位置 
+		for (int j = sx; j < ex; j++) { // 横方向のループ 
+			*pixel++ = color[0]; // B 
+			*pixel++ = color[1]; // G 
+			*pixel++ = color[2]; // R 
+		} 
+	} 
 }
 
-int centerX; // 円の中心座標X
-int centerY; // 円の中心座標Y
-int radius;  // 円の半径
-
-// 初期化処理（最初に1回だけ呼び出される）
-void FrameBufferEmulator::initUser()
-{
-	// フレームバッファの中心座標を求める
-	centerX = width / 2;
-	centerY = height / 2;
-	radius = 100; // 初期の半径
+// 格子を描画する 
+void drawGrids(unsigned char* buff, int width, int height, int w, int h, unsigned char color[3]) 
+{ 
+	// 横線 
+	for (int y = 0; y < height; y += h) { // 縦方向のループ 
+		unsigned char* p = buff + (y * width * 3); // 書き込み始める位置を求める（行） 
+		for (int x = 0; x < width; x++) { // 横方向のループ 
+			*p++ = color[0]; 
+			*p++ = color[1]; 
+			*p++ = color[2]; 
+		} 
+	}
+	
+	// 縦線 
+	for (int x = 0; x < width; x += w) { // 横方向のループ 
+		for (int y = 0; y < height; y++) { // 縦方向のループ 
+			unsigned char* p = buff + ((y * width + x) * 3); // 書き込み始める位置を求める（列） 
+			*p++ = color[0]; 
+			*p++ = color[1]; 
+			*p++ = color[2]; 
+		} 
+	} 
 }
 
-// 描画処理（毎フレーム呼び出される）
-void FrameBufferEmulator::drawUser(unsigned char* buff, int mode, int keyLevel, int keyTrigger)
-{
-	unsigned char color[3] = { 10, 200, 0 }; // B, G, R
-
-	if (keyTrigger == SDLK_UP) { // 上矢印キーが押されたら
-		radius++;  // 半径を大きくする
-	}
-	drawFilledCircle(buff, width, height, radius, centerX, centerY, color); // 円を描画する
+void FrameBufferEmulator::initUser() 
+{ 
+	cnt = 0; 
 }
